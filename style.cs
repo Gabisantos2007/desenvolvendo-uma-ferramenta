476b58a505c0a26c61fac33ac83bfd91a432884f
Gabisantos2007 <141463743+Gabisantos2007@users.noreply.github.com>
2024-10-09 21:43:28 -0300

    kjb

---
+++ style.cs
@@ -0,0 +1,18 @@
+body {
+    background-color: black;
+}
+
+footer {
+    background-color:black
+    color: white;
+    position: fixed
+    bottom: 0;
+    width: 100%;
+    height: 1rem;
+    }
+
+footer p {
+    text-align: center;
+    font-sizw: 0.5rem;
+
+}
